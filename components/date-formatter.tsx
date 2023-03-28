8d8c8365548c8f07817a4aec9ea4c49000b23bc7
94sssh <abizsh@naver.com>
2023-03-28 17:09:08 +0900

    날짜 양식 수정

--- components/date-formatter.tsx
+++ components/date-formatter.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'yyyy.MM.d')}</time>;
+  return <time dateTime={dateString}>{format(date, 'yyyy.MM.dd')}</time>;
 };
 
 export default DateFormatter;
