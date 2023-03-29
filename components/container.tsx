810625956f64e30376f0b4d37602db0c7b103e66
94sssh <abizsh@naver.com>
2023-03-29 10:55:54 +0900

    반응형 조건 수정

--- components/container.tsx
+++ components/container.tsx
@@ -3,7 +3,7 @@ type Props = {
 };
 
 const Container = ({ children }: Props) => {
-  return <div className='w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto px-5 bg-lime-500'>{children}</div>;
+  return <div className='w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto px-5'>{children}</div>;
 };
 
 export default Container;
