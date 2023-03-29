9e6d7d76bf0c0c2d3f37bd4b34a9db456bfb01f6
94sssh <abizsh@naver.com>
2023-03-29 11:58:13 +0900

    post-header 컴포넌트 구성요소 배치 및 폰트 컬러 변경

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -11,16 +11,12 @@ type Props = {
 const PostHeader = ({ title, coverImage, date }: Props) => {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
-      <div className='hidden md:block md:mb-12'></div>
-      <div className='mb-8 md:mb-16 sm:mx-0'>
+      <div className='mb-8 sm:mx-0'>
         <CoverImage title={title} src={coverImage} />
       </div>
-      <div className='max-w-2xl mx-auto'>
-        <div className='block md:hidden mb-6'></div>
-        <div className='mb-6 text-lg'>
-          <DateFormatter dateString={date} />
-        </div>
+      <PostTitle>{title}</PostTitle>
+      <div className='max-w-2xl text-base text-slate-600'>
+        <DateFormatter dateString={date} />
       </div>
     </>
   );
