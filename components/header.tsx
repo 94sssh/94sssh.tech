f53347924cf693fecf0a9244a16c45a6ef49ea7f
94sssh <abizsh@naver.com>
2023-03-29 10:55:25 +0900

    header 수정

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,24 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
-      </Link>
-      .
-    </h2>
-  )
-}
+    <header className='mt-8 mb-20 flex justify-between'>
+      <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight'>
+        <Link href='/'>94sssh</Link>.
+      </h2>
+      <div className='flex justify-center'>
+        <p className='m-2'>
+          <Link href='/'>소개</Link>
+        </p>
+        <p className='m-2'>
+          <Link href='/'>블로그</Link>
+        </p>
+        <p className='m-2'>
+          <Link href='/'>검색</Link>
+        </p>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
