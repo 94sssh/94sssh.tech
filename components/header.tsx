1f611692f92a47d5a782a82638e05836d1f46076
94sssh <abizsh@naver.com>
2023-04-05 23:35:39 +0900

    header 컴포넌트 내 검색 제거

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className='mt-8 mb-20 flex justify-between'>
+    <header className='mt-8 mb-20 flex justify-between sm:max-w-screen-sm md:max-w-screen-md mx-auto'>
       <h2 className='text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight'>
         <Link href='/'>94sssh</Link>.
       </h2>
@@ -12,9 +12,6 @@ const Header = () => {
         </p>
         <p className='m-2'>
           <Link href='/'>블로그</Link>
-        </p>
-        <p className='m-2'>
-          <Link href='/'>검색</Link>
         </p>
       </div>
     </header>
