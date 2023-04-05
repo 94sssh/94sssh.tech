426ed5257d0c253d27654f31d909cc1416dc1817
94sssh <abizsh@naver.com>
2023-04-05 23:36:41 +0900

    footer 컴포넌트 내 container 컴포넌트 제거

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,14 @@
-import Container from './container';
-
 const Footer = () => {
   return (
-    <footer className='bg-white border-t border-neutral-200'>
-      <Container>
-        <div className='py-8 flex lg:flex-row items-center justify-between'>
-          <h3 className='text-2xl lg:text-3xl items-center font-bold tracking-tighter leading-tight lg:mb-0 lg:pr-4 lg:w-1/2'>94sssh TechBlog</h3>
-          <div className='flex lg:flex-row justify-end items-center lg:pl-4'>
-            <a href={`https://github.com/94sssh`} className='mx-3 font-bold'>
-              Visit GitHub
-            </a>
-          </div>
+    <footer className='bg-white border-t border-neutral-200 sm:max-w-screen-sm md:max-w-screen-md mx-auto'>
+      <div className='py-8 flex lg:flex-row items-center justify-between'>
+        <h3 className='text-2xl lg:text-3xl items-center font-bold tracking-tighter leading-tight lg:mb-0 lg:pr-4 lg:w-1/2'>94sssh TechBlog</h3>
+        <div className='flex lg:flex-row justify-end items-center lg:pl-4'>
+          <a href={`https://github.com/94sssh`} className='mx-3 font-bold'>
+            Visit GitHub
+          </a>
         </div>
-      </Container>
+      </div>
     </footer>
   );
 };
