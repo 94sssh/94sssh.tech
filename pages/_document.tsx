26a7f20b5a087c3c5056384666b8124eb3db5789
94sssh <abizsh@naver.com>
2023-04-15 13:46:02 +0900

    코드블럭용 Hack 폰트 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang='en'>
       <Head>
         <link rel='stylesheet' as='style' href='https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard-dynamic-subset.css' />
+        <link rel='stylesheet' href='//cdn.jsdelivr.net/npm/hack-font@3.3.0/build/web/hack.css'></link>
       </Head>
       <body>
         <Main />
